00b20af4e4a41d8962fc47881f9f30e2d5f664d2
cartibhati <mymaimi12@gmail.com>
2026-06-13 09:37:09 +0530

    prefix sum

--- Prefix_Sum/2_subarray_sum_equals_k.cpp
+++ Prefix_Sum/2_subarray_sum_equals_k.cpp
@@ -2,16 +2,16 @@ class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
         int sum=0;
-        int ans=0;
+        int res=0;
         unordered_map<int,int>f;
         f[0]=1;
-        for(int i=0;i<nums.size();i++){
+        for(int i=0 ;i<nums.size();i++){
             sum+=nums[i];
             int ques=sum-k;
             int freq=f[ques];
-            ans+=freq;
+            res+=freq;
             f[sum]++;
         }
-        return ans;
+        return res;
     }
 };
