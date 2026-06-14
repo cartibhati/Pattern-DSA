20b61840a29efbd14ec43b9360558dbf1469dc60
cartibhati <mymaimi12@gmail.com>
2026-06-14 15:32:08 +0530

    practiced sliding windows

--- Sliding_Window/2_min_size_subarray_sum.cpp
+++ Sliding_Window/2_min_size_subarray_sum.cpp
@@ -5,17 +5,16 @@ public:
         int high=0;
         int ans=INT_MAX;
         int sum=0;
-        int n=nums.size();
-        while(high<n){
+        while(high<nums.size()){
             sum=sum+nums[high];
             while(sum>=target){
                 int len=high-low+1;
                 ans=min(ans,len);
-                sum-=nums[low];
+                sum=sum-nums[low];
                 low++;
             }
             high++;
         }
-    return ans == INT_MAX ? 0 : ans;
+        return (ans == INT_MAX) ? 0 : ans;
     }
 };
