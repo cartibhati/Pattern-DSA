76dcf6fe9f0f9f614abe121ee3d67d161aedb509
cartibhati <mymaimi12@gmail.com>
2026-06-14 15:41:36 +0530

    practiced sliding windows

--- Sliding_Window/3_long_substr_with_k_uniques.cpp
+++ Sliding_Window/3_long_substr_with_k_uniques.cpp
@@ -9,17 +9,19 @@ class Solution {
         unordered_map<char,int>f;
         for(int high=0;high<n;high++){
             f[s[high]]++;
+            
             while(f.size()>k){
                 f[s[low]]--;
                 if(f[s[low]]==0)
                 f.erase(s[low]);
                 low++;
             }
-            if (f.size() == k) {
-                ans = max(ans, high - low + 1);
+            
+            if(f.size()==k){
+                int len = high-low+1;
+                ans= max(ans,len);
             }
         }
-
         return ans;
     }
 };
