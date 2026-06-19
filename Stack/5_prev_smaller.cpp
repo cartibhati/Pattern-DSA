e6a066d76ab2c43affc4008e757bb9fa37fe16bf
cartibhati <mymaimi12@gmail.com>
2026-06-19 11:27:27 +0530

    stack pattern

---
+++ Stack/5_prev_smaller.cpp
@@ -0,0 +1,24 @@
+class Solution {
+  public:
+    vector<int> prevSmaller(vector<int>& arr) {
+        int n = arr.size();
+        vector<int> res(n);
+        stack<int> st;
+
+        for (int i = 0; i < n; i++) {
+            while (!st.empty() && st.top() >= arr[i]) {
+                st.pop();
+            }
+
+            if (st.empty())
+                res[i] = -1;
+            else
+                res[i] = st.top();
+
+            st.push(arr[i]);
+        }
+
+        return res;
+        
+    }
+};
