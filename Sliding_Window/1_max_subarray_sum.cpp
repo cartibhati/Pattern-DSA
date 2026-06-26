14b0ee2ae45dcbf22c36fc4cc5f9e3d36c6be5bb
cartibhati <mymaimi12@gmail.com>
2026-06-26 10:15:32 +0530

    Refactor max subarray sum sliding window solution

--- Sliding_Window/1_max_subarray_sum.cpp
+++ Sliding_Window/1_max_subarray_sum.cpp
@@ -2,24 +2,23 @@ class Solution {
   public:
     int maxSubarraySum(vector<int>& arr, int k) {
         // code here
-        int low=0;
-        int high=k-1;
         int n=arr.size();
+        int low=0;
+        int high= k-1;
         int sum=0;
         for(int i=0;i<=high;i++){
-            sum=sum+arr[i];
+            sum+=arr[i];
         }
+        
         int ans=sum;
-       while (high < n - 1) {
-            low++;
+        while(high<n-1){
             high++;
-
-            sum = sum - arr[low - 1];
-            sum = sum + arr[high];
-
-            ans = max(ans, sum);
+            sum += arr[high];
+            sum -= arr[low];
+            low++;
+            
+            ans=max(ans,sum);
         }
         return ans;
-        
     }
 };
