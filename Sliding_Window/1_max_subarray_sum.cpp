eaa04ab2ab40b7f6cb3312c4ce90e796178f43af
Arjun Singh Bhati <mymaimi12@gmail.com>
2026-07-15 21:40:27 +0530

    Improve maxSubarraySum implementation
    Refactor maxSubarraySum function for clarity and efficiency.

--- Sliding_Window/1_max_subarray_sum.cpp
+++ Sliding_Window/1_max_subarray_sum.cpp
@@ -2,23 +2,24 @@ class Solution {
   public:
     int maxSubarraySum(vector<int>& arr, int k) {
         // code here
-        int n=arr.size();
         int low=0;
-        int high= k-1;
+        int high=k-1;
+        int n=arr.size();
         int sum=0;
         for(int i=0;i<=high;i++){
-            sum+=arr[i];
+            sum=sum+arr[i];
         }
-        
         int ans=sum;
-        while(high<n-1){
-            high++;
-            sum += arr[high];
-            sum -= arr[low];
+       while (high < n - 1) {
             low++;
-            
-            ans=max(ans,sum);
+            high++;
+
+            sum = sum - arr[low - 1];
+            sum = sum + arr[high];
+
+            ans = max(ans, sum);
         }
         return ans;
+        
     }
 };
