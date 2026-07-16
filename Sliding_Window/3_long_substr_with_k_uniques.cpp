0590b40f930a810fba2c1b0cd87046033d9baeba
Arjun Singh Bhati <mymaimi12@gmail.com>
2026-07-16 12:46:19 +0530

    Refactor sliding window logic for unique substrings
    Fixed logic for counting unique characters and updated length calculation.

--- Sliding_Window/3_long_substr_with_k_uniques.cpp
+++ Sliding_Window/3_long_substr_with_k_uniques.cpp
@@ -8,20 +8,21 @@ class Solution {
         int ans=-1;
         unordered_map<char,int>f;
         for(int high=0;high<n;high++){
-            f[s[high]]++;
-            
-            while(f.size()>k){
+                f[s[high]]++;
+                
+                while(f.size()>k){
                 f[s[low]]--;
                 if(f[s[low]]==0)
                 f.erase(s[low]);
                 low++;
             }
-            
             if(f.size()==k){
-                int len = high-low+1;
-                ans= max(ans,len);
+                int len=high-low+1;
+                ans=max(ans,len);
             }
         }
+        
         return ans;
+        
     }
 };
