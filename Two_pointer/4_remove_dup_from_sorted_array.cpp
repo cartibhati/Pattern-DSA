51ce17b8371d8425ec83375cb961faff848f04f4
Arjun Singh Bhati <mymaimi12@gmail.com>
2026-08-18 22:00:29 +0530

    Update 4_remove_dup_from_sorted_array.cpp

--- Two_pointer/4_remove_dup_from_sorted_array.cpp
+++ Two_pointer/4_remove_dup_from_sorted_array.cpp
@@ -3,10 +3,11 @@ public:
     int removeDuplicates(vector<int>& nums) {
         int i=1;
         int j=1;
-        while(j<nums.size()){
+        int n=nums.size();
+        while(i<n && j<n){
             if(nums[j]!=nums[i-1]){
                 nums[i]=nums[j];
-                i++; 
+                i++;
             }
             j++;
         }
