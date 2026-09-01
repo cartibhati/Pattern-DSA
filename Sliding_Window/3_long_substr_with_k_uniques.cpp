7c76b3ceeaf77c85097ea455207f2cc51acf5f67
Arjun Singh Bhati <mymaimi12@gmail.com>
2026-09-01 21:15:20 +0530

    Refactor longestKSubstr function for clarity

--- Sliding_Window/3_long_substr_with_k_uniques.cpp
+++ Sliding_Window/3_long_substr_with_k_uniques.cpp
@@ -3,26 +3,24 @@ class Solution {
     int longestKSubstr(string &s, int k) {
         // code here
         int low=0;
-        int high=0;
         int n=s.size();
-        int ans=-1;
+        int high=0;
+        int res=-1;
         unordered_map<char,int>f;
-        for(int high=0;high<n;high++){
-                f[s[high]]++;
-                
-                while(f.size()>k){
+        for(high=0;high<n;high++){
+            f[s[high]]++;
+            while(f.size()>k){
                 f[s[low]]--;
                 if(f[s[low]]==0)
                 f.erase(s[low]);
                 low++;
             }
+            
             if(f.size()==k){
                 int len=high-low+1;
-                ans=max(ans,len);
+                res=max(res,len);
             }
         }
-        
-        return ans;
-        
+        return res;
     }
 };
