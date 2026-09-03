b8ae122d4ba718fb5905cc09a5c12c789550d398
Arjun Singh Bhati <mymaimi12@gmail.com>
2026-09-03 21:27:52 +0530

    Refactor cycle detection in linked list

--- Slow_Fast_pointer/2_Linked_List_cycle_2.cpp
+++ Slow_Fast_pointer/2_Linked_List_cycle_2.cpp
@@ -1,15 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
         ListNode* slow = head;
         ListNode* fast = head;
 
-        // Find meeting point
-        while (fast!=NULL && fast->next!=NULL) {
+        // Step 1: Detect cycle
+        while (fast != NULL && fast->next != NULL) {
             slow = slow->next;
             fast = fast->next->next;
 
             if (slow == fast) {
+                // Step 2: Find the starting node of the cycle
                 slow = head;
 
                 while (slow != fast) {
@@ -17,10 +27,10 @@ public:
                     fast = fast->next;
                 }
 
-                return slow; // start of cycle
+                return slow;
             }
         }
 
-        return NULL; // no cycle
+        return NULL;
     }
 };
