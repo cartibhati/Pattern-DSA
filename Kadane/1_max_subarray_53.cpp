5795b1b068554ba33b0b25bc88246a4e8c5996e4
Arjun Singh Bhati <mymaimi12@gmail.com>
2026-09-04 21:15:19 +0530

    Refactor maxSubArray to improve variable usage

--- Kadane/1_max_subarray_53.cpp
+++ Kadane/1_max_subarray_53.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int i=0; 
         int bestend=nums[0];
         int ans=nums[0];
         for(int i=1;i<nums.size();i++){
-            int v1=bestend+nums[i];
-            int v2=nums[i];
+            int v1=nums[i];
+            int v2=bestend+nums[i];
             bestend=max(v1,v2);
             ans=max(ans,bestend);
         }
